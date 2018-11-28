0ee9c26a82cab25ca7663dbf93d39844786d1ea9
Bela Toth <tbela@inf.u-szeged.hu>
2018-11-28 08:51:32 +0100

    Add and iotjs client example to wit-mqtt
    If `port` and `host` are set to the server's correspoding data, the client script will catch,
    and parse the published json into it's order array, based on the originally configured wit intents, and values.

---
+++ iotjs_mqtt_client.js
@@ -0,0 +1,46 @@
+var mqtt = require('mqtt');
+
+//connecting options
+var opts = {
+  port: 4444,
+  keepalive: 1000,
+  clientId: 'mqtt_bela',
+  host: '127.0.0.1'
+}
+
+//subscribe_options
+var subscribe_opts = {
+  retain: false,
+  qos: 2
+}
+
+var client = mqtt.connect( opts, function () {
+  console.log("client connected");
+  client.subscribe('hello/iotjs', subscribe_opts, function(error) {
+    if (error) {
+      console.log('Subscribe is failed');
+    } else {
+      console.log('Message has been published');
+    }
+  });
+});
+
+client.on('message', function(data) {
+  var orders = []
+  console.log('I received something: ' + data.message.toString());
+  data = JSON.parse(data.message)
+  Object.keys(data.entities).forEach( function each(element) {
+    //console.log(data.entities[element]);
+    Object.keys(data.entities[element]).forEach( function each(property) {
+      console.log(data.entities[element][property].value)
+      orders.push(data.entities[element][property].value)
+    });
+  });
+  console.log(orders);
+});
+
+client.on('error', function(data){
+  console.log(data);
+})
+
+
